64471aa6e9c1fe6477d5ad73036a9d7cdcd86ab2
brennangw <brennangoffwallace@gmail.com>
2014-08-07 16:48:16 -0500

    Initial Commit

---
+++ mathSkills/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  mathSkills
+//
+//  Created by Brennan Goff Wallace on 8/7/14.
+//  Copyright (c) 2014 Brennan Goff Wallace. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
